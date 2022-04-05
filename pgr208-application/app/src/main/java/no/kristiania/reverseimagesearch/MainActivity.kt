d81224749532d36d42841ef0551dce8e73c3dfaf
mariestigen <marie.stigen@hotmail.com>
2022-04-05 12:27:25 +0200

    Can pick image from gallery

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/MainActivity.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/MainActivity.kt
@@ -1,11 +1,44 @@
 package no.kristiania.reverseimagesearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var uploadBtn: Button
+    private lateinit var imagePreview: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        uploadBtn = findViewById(R.id.upload_btn)
+        imagePreview = findViewById(R.id.uploaded_image)
+
+        uploadBtn.setOnClickListener {
+            pickImageGallery()
+        }
     }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        resultLauncher.launch(intent)
+
+    }
+
+    private val resultLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                imagePreview.setImageURI(result.data?.data)
+            } else {
+                println("U ARE FUKD")
+            }
+        }
 }
