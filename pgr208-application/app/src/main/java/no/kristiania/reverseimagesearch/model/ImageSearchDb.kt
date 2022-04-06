79445d86e4de62b279a6c5e14adbad78314c5d30
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-06 12:59:50 +0200

    Added ImageSearchDb

---
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/model/ImageSearchDb.kt
@@ -0,0 +1,31 @@
+package no.kristiania.reverseimagesearch.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [RequestImage::class], version = 1, exportSchema = false)
+abstract class ImageSearchDb : RoomDatabase() {
+    abstract val requestImageDao: RequestImageDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ImageSearchDb? = null
+
+        fun getInstance(context: Context): ImageSearchDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ImageSearchDb::class.java,
+                        "image_search_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
