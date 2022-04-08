c7f11c56de0626e5bb6ecfd486011d08d02bd921
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-08 12:02:05 +0200

    Oppdatert ResultItemAdapter til å benytte list adapter

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/view/adapter/ResultItemAdapter.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/view/adapter/ResultItemAdapter.kt
@@ -6,13 +6,12 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import no.kristiania.reverseimagesearch.R
+import no.kristiania.reverseimagesearch.databinding.ResultItemBinding
 import no.kristiania.reverseimagesearch.model.entity.ResultItem
 
 
 // Denne klassen forteller recyclerview hvordan den skal vise data fra databasen.
 class ResultItemAdapter : ListAdapter<ResultItem, ResultItemAdapter.ResultItemViewHolder>(ResultDiffItemCallback()) {
-
-    var data = listOf<ResultItem>()
 
     // Når den indre klassen under instansieres (dette fungerer som et rot-element for å stappe result_item xml-fila inn i.
     // Den blir inflatet i den indre klassen
@@ -21,26 +20,22 @@ class ResultItemAdapter : ListAdapter<ResultItem, ResultItemAdapter.ResultItemVi
     // Denne kalles for hver gang en recyclerview blir opprettet eller brukt på nytt,
     // for å legge til data i viewet. Dette skjer også i den indre klassen under
     override fun onBindViewHolder(holder: ResultItemViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
     // denne klassen har ansvar for å legge til data i hvert result_item.xml som benyttes i recyclerviewet, samt å inflate de
-    class ResultItemViewHolder(rootView: CardView) : RecyclerView.ViewHolder(rootView){
-        // TODO: 06.04.2022
-        //  variabler til bilde-view, eventuelt annen tekst. Trenger vel på sikt både thumbnail og mulighet til expand,
-        //  ser for meg at vi har et egen fragment til fullscreen view av bilde men idk
+    class ResultItemViewHolder(val binding: ResultItemBinding) : RecyclerView.ViewHolder(binding.root){
 
         companion object {
             fun inflateFrom(parent: ViewGroup): ResultItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.result_item, parent, false) as CardView
-                return ResultItemViewHolder(view)
+                val binding = ResultItemBinding.inflate(layoutInflater, parent, false)
+                return ResultItemViewHolder(binding)
             }
         }
         fun bind(resultItem: ResultItem) {
-            // TODO: sett variablene fra to-do ^over^, til verdien av result item som kommer fra databasen og inn her
+           binding.resultItem = resultItem
         }
     }
 
