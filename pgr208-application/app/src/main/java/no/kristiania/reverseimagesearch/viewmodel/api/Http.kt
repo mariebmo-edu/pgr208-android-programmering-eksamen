209c23928752bbde0cc333d94b838d85bd2a7588
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-12 11:50:51 +0200

    endret til getAsString

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/viewmodel/api/Http.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/viewmodel/api/Http.kt
@@ -1,45 +1,42 @@
 package no.kristiania.reverseimagesearch.viewmodel.api
 
 import android.app.Application
-import android.net.Uri
+import android.content.Context
+import android.graphics.Bitmap
 import com.jacksonandroidnetworking.JacksonParserFactory
 
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import no.kristiania.reverseimagesearch.viewmodel.utils.BitmapUtils
+import no.kristiania.reverseimagesearch.viewmodel.utils.Endpoints
 
-import org.json.JSONObject
-
-import com.androidnetworking.interfaces.JSONObjectRequestListener
-import java.io.File
-
-
-class Http : Application(){
+class Http : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.setParserFactory(JacksonParserFactory())
     }
+    
+    fun uploadImage(bitmap: Bitmap, context: Context) {
 
-    fun uploadImage(image : File){
-        AndroidNetworking.upload("http://api-edu.gtl.ai/ api/v1/imagesearch/upload")
-            .addMultipartFile("image", image)
+        val file = BitmapUtils.bitmapToFile(bitmap, "image.png", context)
+
+        AndroidNetworking.upload(Endpoints.upload_url)
+            .addMultipartFile("image", file)
             .addMultipartParameter("key", "value")
             .setTag("uploadTest")
             .setPriority(Priority.HIGH)
             .build()
-            .getAsJSONObject(object : JSONObjectRequestListener {
-                override fun onResponse(response: JSONObject) {
-                    // do anything with response
+            .getAsString(object : StringRequestListener {
+                override fun onResponse(response: String?) {
                     println(response)
                 }
-
-                override fun onError(error: ANError) {
-                    // handle error
-                    println("upload error!!")
+                override fun onError(error: ANError?) {
+                    println("upload error: " + error?.localizedMessage)
                 }
             })
-
     }
 }
