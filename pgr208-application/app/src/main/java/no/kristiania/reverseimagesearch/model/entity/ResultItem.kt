a359eb1e5210bd532a287daf358193760f65e666
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-12 11:25:48 +0200

    oppdatert notater

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/model/entity/ResultItem.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/model/entity/ResultItem.kt
@@ -5,10 +5,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class ResultItem(val url: String,
-                      @PrimaryKey(autoGenerate = true)
-                      @ColumnInfo(name="result_item_id")
-                      val id: Long? = null
+data class ResultItem(
+    val url: String,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "result_item_id")
+    val id: Long? = null
 )
 
 
