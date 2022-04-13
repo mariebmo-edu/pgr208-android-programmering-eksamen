31ab23c2c6eab19b5c98a6316d12cebc88858bf5
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-13 13:42:59 +0200

    weight

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/model/entity/DummyImagePaths.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/model/entity/DummyImagePaths.kt
@@ -2,7 +2,7 @@ package no.kristiania.reverseimagesearch.model.entity
 
 object DummyImagePaths {
     val IMAGES = hashMapOf<String, String>(
-        "DIE" to "https://en.wikipedia.org/wiki/Portable_Network_Graphics#/media/File:PNG_transparency_demonstration_1.png",
+        "DIE" to "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png",
         "TUX" to "https://i.stack.imgur.com/ILTQq.png",
         "SNOOPIE" to "https://e1.pngegg.com/pngimages/298/527/png-clipart-snoopy.png",
         "MARIO" to "https://www.picng.com/upload/mario/png_mario_75809.png"
