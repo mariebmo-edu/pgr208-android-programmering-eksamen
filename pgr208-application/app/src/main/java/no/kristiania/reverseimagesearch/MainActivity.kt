bdda14b0271f383bf5d3db1c029f76898059d867
Christian Gregersen <chgr007@student.kristiania.no>
2022-04-14 13:57:55 +0200

    refactoring stuff from activity_main.xml into fragment_image_search.xml

--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/MainActivity.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/MainActivity.kt
@@ -11,45 +11,14 @@ import no.kristiania.reverseimagesearch.view.fragment.NothingSelectedFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var savedBtn: Button
-    private lateinit var uploadBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        savedBtn = findViewById(R.id.saved_btn)
-        uploadBtn = findViewById(R.id.upload_btn)
-
-        uploadBtn.setOnClickListener {
-            pickImageGallery()
-        }
-
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.fragmentContainerView, NothingSelectedFragment())
-            addToBackStack(null)
-            commit()
-        }
     }
 
-    private fun pickImageGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        resultLauncher.launch(intent)
-    }
 
-    private val resultLauncher  =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK && it.data != null) {
-
-                supportFragmentManager.beginTransaction().apply{
-                    replace(R.id.fragmentContainerView, ImageSearchFragment.newInstance(it.data?.data!!))
-                    addToBackStack(null)
-                    commit()
-                }
-
-            } else {
-                println("error: result is not OK, or data is empty")
-            }
-        }
 }
--- pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/view/fragment/ImageSearchFragment.kt
+++ pgr208-application/app/src/main/java/no/kristiania/reverseimagesearch/view/fragment/ImageSearchFragment.kt
@@ -1,14 +1,18 @@
 package no.kristiania.reverseimagesearch.view.fragment
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import no.kristiania.reverseimagesearch.R
 import no.kristiania.reverseimagesearch.viewmodel.api.FastNetworkingAPI
 import no.kristiania.reverseimagesearch.viewmodel.utils.BitmapUtils
@@ -16,7 +20,8 @@ import no.kristiania.reverseimagesearch.viewmodel.utils.BitmapUtils.Companion.Ur
 
 
 class ImageSearchFragment : Fragment() {
-
+    private lateinit var savedBtn: Button
+    private lateinit var uploadBtn: Button
     private lateinit var searchBtn: Button
     private lateinit var imagePreview: ImageView
     private lateinit var selectedImage: Bitmap
@@ -34,17 +39,26 @@ class ImageSearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_image_search, container, false)
-
-        searchBtn = view.findViewById<Button>(R.id.search_btn)
+        savedBtn = view.findViewById(R.id.saved_btn)
+        uploadBtn = view.findViewById(R.id.upload_btn)
+        searchBtn = view.findViewById(R.id.search_btn)
         imagePreview = view.findViewById(R.id.uploaded_image)
-
+        uploadBtn.setOnClickListener {
+            pickImageGallery()
+        }
         if(uri != null){
             selectedImage = BitmapUtils.getBitmap(requireContext(), null, uri.toString(), ::UriToBitmap)
             imagePreview.setImageBitmap(selectedImage)
         }
-
+//        supportFragmentManager.beginTransaction().apply{
+//            replace(R.id.fragmentContainerView, NothingSelectedFragment())
+//            addToBackStack(null)
+//            commit()
+//        }
         return view
     }
 
@@ -76,4 +90,24 @@ class ImageSearchFragment : Fragment() {
         val response = context?.let { http.uploadImage(bitmap, it) }
         println("response: $response")
     }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        resultLauncher.launch(intent)
+    }
+
+    private val resultLauncher  =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == AppCompatActivity.RESULT_OK && it.data != null) {
+
+//                supportFragmentManager.beginTransaction().apply{
+//                    replace(R.id.fragmentContainerView, ImageSearchFragment.newInstance(it.data?.data!!))
+//                    addToBackStack(null)
+//                    commit()
+//                }
+
+            } else {
+                println("error: result is not OK, or data is empty")
+            }
+        }
 }
